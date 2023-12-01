acfc7e4d611aea3481f38f78a57dd838bf55007a
iamfelipinho <123705772+iamfelipinho@users.noreply.github.com>
2023-11-30 22:05:16 -0300

    Delete componentes directory

--- componentes/Menu.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>          
-        <NuxtLink to="/">login</NuxtLink> <br/>
-        <NuxtLink to="/tarefas">tarefas</NuxtLink> <br/>
-        <NuxtLink to="/tarefasDet">tarefasDet</NuxtLink> <br/>
-        <NuxtLink to="/ambientes">ambientes</NuxtLink> <br/>
-        <NuxtLink to="/cargos">cargos</NuxtLink> <br/>
-    </div>  
-</template>
